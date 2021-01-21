b6b9a1617e67b3b42bc8459f9ad20294c55f7a4a
F7Alexa <fsetragniset@gmail.com>
2021-01-21 17:22:48 -0300

    update

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from accounts import views as accounts_views
+from boards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pokemaoc/', include('pokemaoc.urls'))
+    path('accounts/', include('accounts.url'))
 ]
